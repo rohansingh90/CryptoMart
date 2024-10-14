2ba12976512689f0a64c6b9c1963b88e1628b00c
rohan singh <rohankardam44@gmail.com>
2024-10-14 13:30:58 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  build: {
+    rollupOptions: {
+      external: ['framer-motion/client'], // Add this line
+    },
+  },
 })
+
+// vite.config.js or vite.config.ts
+import { defineConfig } from 'vite';
+
+
